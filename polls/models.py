7507b909903a26d2952f4902aabe61d68f2dd8ac
Yash-Lade <yashlade2004@gmail.com>
2025-10-30 13:51:01 +0530

    done till render of tutorial 3

--- polls/models.py
+++ polls/models.py
@@ -6,6 +6,7 @@ import datetime
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+    
     def __str__(self):              # this function is used to chnage the represntation format of the table item
         return self.question_text   # as without this if we check through shell then we'll get sth like - <QuerySet [<Question: Question object (1)>]>
     def was_published_recently(self):
@@ -16,4 +17,4 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length= 200)
     votes = models.IntegerField(default=0)
     def __str__(self):
-        return self.choice_text
+        return self.choice_texts
